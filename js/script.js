585073d4a1c71804f093b11953f0dd7c0efdc026
Aaron Faucher <aaron.faucher@gmail.com>
2016-12-12 21:10:18 -0800

    Fixed no-tabbing issue

--- js/script.js
+++ js/script.js
@@ -647,46 +647,46 @@ to project pages.
 
 ––––––––––––––––––––––––––––––*/
 
-$('#p001 #projects ul li.project').mouseenter(function(){
+// $('#p001 #projects ul li.project').mouseenter(function(){
 
-	//console.log("ENTER");
-	// $(this).addClass('focus');
-	$(this).addClass('pointer');
-	$(this).fadeTo(400,'1');
+// 	//console.log("ENTER");
+// 	// $(this).addClass('focus');
+// 	$(this).addClass('pointer');
+// 	$(this).fadeTo(400,'1');
 
-});
+// });
 
-$('#p001 #projects ul li.project').mouseleave(function(){
+// $('#p001 #projects ul li.project').mouseleave(function(){
 
-	//console.log("LEAVE");
-	// $(this).removeClass('focus');
-	$(this).removeClass('pointer');
-	$(this).fadeTo(300,'.4');
+// 	//console.log("LEAVE");
+// 	// $(this).removeClass('focus');
+// 	$(this).removeClass('pointer');
+// 	$(this).fadeTo(300,'.4');
 
-});
+// });
 
 
-$('#p001 #projects ul li.project').click(function(){
+// $('#p001 #projects ul li.project').click(function(){
 
-	// did the click work?
-	//console.log("CLICKED");
+// 	// did the click work?
+// 	//console.log("CLICKED");
 
-	// create a var to hold the projectName
-	var projectName = $(this).data('name');
+// 	// create a var to hold the projectName
+// 	var projectName = $(this).data('name');
 
-	// log the projectName, just to be sure
-	//console.log(projectName);
+// 	// log the projectName, just to be sure
+// 	//console.log(projectName);
 
-	// call the 'goToUrl' function
-	goToUrl(baseUrl,'/projects/'+projectName+'/');
+// 	// call the 'goToUrl' function
+// 	goToUrl(baseUrl,'/projects/'+projectName+'/');
 
-	// assemble a url with the projectName
-	//var projectUrl = "http://asf-p-a.pancakeapps.com/projects/" + projectName + "/";
+// 	// assemble a url with the projectName
+// 	//var projectUrl = "http://asf-p-a.pancakeapps.com/projects/" + projectName + "/";
 
-	// go to that URL
-	//window.location.href = projectUrl;
+// 	// go to that URL
+// 	//window.location.href = projectUrl;
 
-});
+// });
 
 
 
